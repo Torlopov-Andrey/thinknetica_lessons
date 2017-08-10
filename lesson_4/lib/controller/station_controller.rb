c3bbb51c49adb6a5757ceaa1842368afa4b87325
Andrey Torlopov <torlopov.andrey@gmail.com>
2017-08-10 14:47:06 +0300

    lesson 4. Trainspotting simulator

---
+++ lesson_4/lib/controller/station_controller.rb
@@ -0,0 +1,40 @@
+require_relative './../model/station'
+require_relative 'controller'
+
+module Trainspotting 
+  class StationController < Controller
+    def initialize
+      super
+      @type = 'station'
+    end
+
+    protected 
+
+    def show
+      puts
+      @data.each_with_index do |station,index|
+        puts "#{index+1}. Station: #{station.name} :: train count: #{station.trains.count}"
+      end
+      puts "Station list is empty" if @data.empty?
+    end
+
+    def add
+      system("clear")
+      puts
+      puts 'Input station name'
+      print '> '
+      name = gets.chomp
+      return if @data.select {|station| station.name == name}.count > 0
+      @data << Station.new(name) unless name.empty?
+    end
+
+    def remove
+      system("clear")
+      self.show
+      puts 'Type station index to delete:'
+      print '> '
+      i = gets.chomp.to_i
+      @data.delete_at(i)
+    end
+  end
+end
